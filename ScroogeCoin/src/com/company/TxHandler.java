f37a4e29ac74879f16a9690f5e7197e07ba73703
BlaineMalone <bmalone05@qub.ac.uk>
2017-09-06 21:14:38 +0100

    First Assigment complete

--- ScroogeCoin/src/com/company/TxHandler.java
+++ ScroogeCoin/src/com/company/TxHandler.java
@@ -12,22 +12,25 @@ public class TxHandler {
      * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
      * constructor.
      */
-    public TxHandler(UTXOPool utxoPl) {
-        utxoPool = new UTXOPool(utxoPl);
+    public TxHandler(UTXOPool utxoPool) {
+        this.utxoPool = new UTXOPool(utxoPool);
     }
 
     /**
      * @return true if:
-     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
-     * (2) the signatures on each input of {@code tx} are valid, 
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool,
+     * (2) the signatures on each input of {@code tx} are valid,
      * (3) no UTXO is claimed multiple times by {@code tx},
      * (4) all of {@code tx}s output values are non-negative, and
      * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
+        if (tx == null) {
+            return false;
+        }
         claimedUTXOs = new UTXOPool();
-        int sumOfInputValues = 0;
+        double sumOfInputValues = 0;
         for(int i = 0; i < tx.numInputs(); i++) {
             Transaction.Input in = tx.getInput(i);
             UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
@@ -39,10 +42,9 @@ public class TxHandler {
             claimedUTXOs.addUTXO(utxo, output);
             sumOfInputValues += output.value;
         }
-        ArrayList<Transaction.Output> outputs = tx.getOutputs();
-        int sumOfOutputValues = 0;
-        for(Transaction.Output output : outputs) {
-            if(output.value<0) return false;
+        double sumOfOutputValues = 0;
+        for(Transaction.Output output : tx.getOutputs()) {
+            if(output.value < 0) return false;
             sumOfOutputValues += output.value;
         }
         return sumOfInputValues >= sumOfOutputValues;
@@ -54,25 +56,24 @@ public class TxHandler {
      * updating the current UTXO pool as appropriate.
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
+        if (possibleTxs == null) {
+            return new Transaction[0];
+        }
         Set<Transaction> validBlock = new HashSet<>();
-        for(int i = 0; i < possibleTxs.length; i++) {
-            Transaction transaction = possibleTxs[i];
-            //Check if tx is valid - in isolation
-            if(isValidTx(transaction)) {
-                validBlock.add(possibleTxs[i]);
-                //Remove UTXO from pool that has been spent in valid tx
-                ArrayList<UTXO> txUTXOs = claimedUTXOs.getAllUTXO();
-                for(int j = 0; i < txUTXOs.size(); j++) {
-                    utxoPool.removeUTXO(txUTXOs.get(j));
-                    //Create UTXO with tx hash and index of output
-                    UTXO utxo = new UTXO(transaction.getHash(), j);
-                    //Add UTXO to utxoPool
-                    utxoPool.addUTXO(utxo, transaction.getOutput(j));
+        for(Transaction tx : possibleTxs) {
+            if (isValidTx(tx)) {
+                validBlock.add(tx);
+                ArrayList<UTXO> claimed = claimedUTXOs.getAllUTXO();
+                for (UTXO utxo : claimed) {
+                    utxoPool.removeUTXO(utxo);
+                }
+                for (int i = 0; i < tx.numOutputs(); i++) {
+                    Transaction.Output out = tx.getOutput(i);
+                    UTXO utxo = new UTXO(tx.getHash(), i);
+                    utxoPool.addUTXO(utxo, out);
                 }
             }
         }
-        Transaction[] validBlockArray = new Transaction[validBlock.size()];
-        return validBlock.toArray(validBlockArray);
+        return validBlock.toArray(new Transaction[validBlock.size()]);
     }
-
 }
